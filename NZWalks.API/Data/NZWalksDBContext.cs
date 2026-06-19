56204602673947c3914384e4adce544320a5a911
umathileepan <thileepan_uma@yahoo.co.uk>
2026-06-19 16:41:52 +0100

    Created Model folder which has three classes, Added DBContext and Connection String.

---
+++ NZWalks.API/Data/NZWalksDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Model.Domain;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksDBContext : DbContext
+    {
+        public NZWalksDBContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+               
+        }
+
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Regions> Regions { get; set; }
+        public DbSet<Walks> Walks { get; set; }
+    }
+}
